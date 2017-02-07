8d8fbffa96e96d869854d7711e14a9730538ecf7
@rafszul <rafszul@gmail.com>
2017-02-07 06:00:17 +0000

    chat wired

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DemoComponent } from './demo/demo.component';
 import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const router: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'demo', component: DemoComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'chat', component: ChatComponent }
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -7,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire, private router: Router) {
+
+    this.items = af.database.list('/messages', {
+      query: {
+        limitToLast: 20
+      }
+    });
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+
+  }
+
+  logout() {
+    this.af.auth.logout();
+    console.log('logged out');
+    this.router.navigateByUrl('/login');
+  }
+
+  chatSend(theirMessage: string) {
+    this.items.push({ message: theirMessage, name: this.name.auth.displayName });
+    this.msgVal = '';
+  }
 
   ngOnInit() {
   }
