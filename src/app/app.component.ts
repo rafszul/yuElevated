28a732082564a4f539cbba3cc92ba92c67b4d6c6
rafszul <rafszul@gmail.com>
2017-02-14 12:00:32 +0000

    qwerty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 }
+
+// import {Component, ViewEncapsulation} from '@angular/core';
+
+
+// @Component({
+//   selector: 'sidenav-fab-example',
+//   templateUrl: './sidenav-fab-example.html',
+//   styleUrls: ['./sidenav-fab-example.css'],
+//   encapsulation: ViewEncapsulation.None,
+// })
+// export class SidenavFabExample {}
+
+// import {Component, ViewEncapsulation} from '@angular/core';
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+//   encapsulation: ViewEncapsulation.None,
+// })
+// export class AppComponent {}
